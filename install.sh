ea50b4490f9850dfb512d59d5f7d78542ccafe24
Alina Iv <alina.ivontzyna@yandex.ru>
2026-07-03 16:22:55 +0700

    Fix REAME and install.sh text

--- install.sh
+++ install.sh
@@ -4,59 +4,35 @@ set -e
 
 cd "$(dirname "$0")"
 
-echo "=== Package Version Monitor installation ==="
+echo "=== Установка Package Version Monitor ==="
 
-# Проверка Python
-if ! command -v python3 >/dev/null 2>&1; then
-    echo "Error: python3 is not installed."
-    exit 1
-fi
+echo "Обновление списка пакетов..."
+sudo apt update
 
-# Проверка Ansible
-if ! command -v ansible-playbook >/dev/null 2>&1; then
-    echo "Error: ansible-playbook is not installed."
-    exit 1
-fi
+echo "Установка зависимостей..."
+sudo apt install -y \
+    ansible \
+    python3 \
+    python3-pip \
+    python3-venv
 
-# Проверка venv
-if ! python3 -m venv --help >/dev/null 2>&1; then
-    echo "Error: Python venv module is not available."
-    echo
-    echo "Ubuntu/WSL:"
-    echo "    sudo apt install python3-venv"
-    echo
-    exit 1
-fi
+echo "Создание виртуального окружения..."
 
-# Создание окружения
 if [ ! -x "compare/venv/bin/python" ]; then
-    echo "Creating virtual environment..."
     python3 -m venv compare/venv
 fi
 
-# Проверка pip внутри venv
-if [ ! -x "compare/venv/bin/pip" ]; then
-    echo "Error: pip is not available in the virtual environment."
-    echo
-    echo "Install it using:"
-    echo "    sudo apt install python3-pip"
-    exit 1
-fi
+echo "Установка Python-зависимостей..."
+compare/venv/bin/python -m pip install -r compare/requirements.txt
 
-echo "Installing Python dependencies..."
-compare/venv/bin/pip install -r compare/requirements.txt
-
-echo "Making scripts executable..."
+echo "Выдача прав на выполнение..."
 chmod +x run.sh
 chmod +x scripts/get_versions.sh
 
 echo
-echo "Installation completed."
+echo "Установка завершена."
 echo
-echo "Next steps:"
-echo "  1. Configure inventory/hosts.ini"
-echo "  2. Configure SSH access to remote hosts"
-echo "  3. Add run.sh to crontab"
-echo
-echo "Example cron job: (job starts at 12 am)"
-echo "0 12 * * * $(pwd)/run.sh" 
+echo "Дальнейшие действия:"
+echo "1. Заполните inventory/hosts.ini."
+echo "2. Настройте SSH-доступ до удалённых машин."
+echo "3. Запустите ./run.sh или добавьте его в cron."
